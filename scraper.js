d9acad60a16f6379d3dc0a6af5496605d11b43ce
Rehan-Shah12 <100332064+Rehan-Shah12@users.noreply.github.com>
2023-05-13 16:07:31 +0500

    Faster Scrapper Updated

--- scraper.js
+++ scraper.js
@@ -1,5 +1,5 @@
 import puppeteer from "puppeteer";
-import cheerio from "cheerio";
+import { load } from "cheerio";
 import express from "express";
 import bodyParser from "body-parser";
 import fs from "fs";
@@ -9,28 +9,34 @@ const app = express.Router();
 app.use(bodyParser.json());
 
 app.post("/scrape-search", async (req, res) => {
+  console.log("scrape-search started");
+  console.time("scrape-search");
   const query = req.body.query;
   console.log(query);
 
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: "new",
     ignoreDefaultArgs: ["--disable-extentions"],
     args: ["--no-sandbox"],
   });
+  console.time("browser launch");
 
   const [eloProducts, darazProducts, ishoppingProducts] = await Promise.all([
     elo(query, browser),
     daraz(query, browser),
     ishopping(query, browser),
   ]);
+  console.timeEnd("browser launch");
 
   await browser.close();
+  console.timeEnd("scrape-search");
 
   const searchResults = [
     ...darazProducts,
     ...eloProducts,
     ...ishoppingProducts,
   ];
+  console.timeEnd("scrape-search");
 
   const jsonData = JSON.stringify(searchResults);
   fs.writeFileSync("search_data.json", jsonData);
@@ -45,67 +51,56 @@ export default app;
 ///////////////////////////////////////////////////////////////////////////////////
 
 async function elo(query, browser) {
+  console.log("elo started");
+  console.time("elo");
+
   const page = await browser.newPage();
   page.setDefaultNavigationTimeout(0);
 
-  // Overrides the hieghts property
   const override = Object.assign(page.viewport(), { width: 1366 });
   await page.setViewport(override);
 
-  // This gives an error for no height
-  // const width = 1024;
-  // await page.setViewport({ width: width });
   await page.setUserAgent("UA-TEST");
 
-  const url = "https://www.exportleftovers.com/";
+  const url = `https://www.exportleftovers.com/search?q=${query}`;
   await page.goto(url);
 
-  // waiting for search bar to load
-  await page.waitForSelector(
-    "div.control.header__search-bar.is-relative > input"
-  );
-  await page.type("div.control.header__search-bar.is-relative > input", query);
-  await page.keyboard.press("Enter");
-
-  await page.waitForNavigation();
-  // await page.waitForSelector(
-  //   "#shopify-section-template--16146192761011__main > section > div.container.search__content > main > div.search__results-list > div"
-  // );
   await page.waitForSelector("div.search__item.container.has-padding-bottom");
-  //   await page.waitForSelector("div.search__item.container.has-padding-bottom");
 
   const html = await page.content();
-  const $ = cheerio.load(html);
+  const $ = load(html);
 
   const products = [];
 
-  $("div.search__item.container.has-padding-bottom").each((index, element) => {
-    const name = $(element).find("h3.search-result__title > a").text();
-    const price = $(element).find("span.price > span.money").text().trim();
-    const orignalprice = $(element)
-      .find("span.compare-at-price > span.money")
-      .text()
-      .trim();
-    const image = $(element)
-      .find("div.image-element__wrap > img")
-      .attr("data-src");
-    const desc = $(element).find("div.has-padding-top > p").text().trim();
-    const halflink = $(element)
-      .find("h3.search-result__title > a")
-      .attr("href");
-    const link = "https://www.exportleftovers.com" + halflink;
-    const site = "Elo";
-    products.push({ name, price, orignalprice, image, desc, link, site });
-  });
-
-  // await browser.close();
-
+  $("div.search__item.container.has-padding-bottom:lt(10)").each(
+    (index, element) => {
+      const name = $(element).find("h3.search-result__title > a").text();
+      const price = $(element).find("span.price > span.money").text().trim();
+      const orignalprice = $(element)
+        .find("span.compare-at-price > span.money")
+        .text()
+        .trim();
+      const image = $(element)
+        .find("div.image-element__wrap > img")
+        .attr("data-src");
+      const desc = $(element).find("div.has-padding-top > p").text().trim();
+      const halflink = $(element)
+        .find("h3.search-result__title > a")
+        .attr("href");
+      const link = "https://www.exportleftovers.com" + halflink;
+      const site = "Elo";
+      products.push({ name, price, orignalprice, image, desc, link, site });
+    }
+  );
+  console.timeEnd("elo");
   return products;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////
 
 async function daraz(query, browser) {
+  console.log("Daraz started");
+  console.time("Daraz");
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
 
@@ -117,24 +112,17 @@ async function daraz(query, browser) {
   // const width = 1024;
   // await page.setViewport({ width: width });
   await page.setUserAgent("UA-TEST");
-  const url = "https://www.daraz.pk/";
+
+  const url = `https://www.daraz.pk/catalog/?q=${encodeURIComponent(query)}`;
   await page.goto(url);
 
-  // waiting for search bar to load
-  await page.waitForSelector(".search-box__input--O34g");
-  await page.type(".search-box__input--O34g", query);
-
-  //waiting for the search button to show up
-  await page.waitForSelector(".search-box__search--2fC5");
-  await page.click(".search-box__search--2fC5");
-  await page.waitForNavigation();
-
   const html = await page.content();
-  const $ = cheerio.load(html);
+  const $ = load(html);
 
   const products = [];
+  await page.waitForSelector("div.inner--SODwy");
 
-  $("div.inner--SODwy").each((index, element) => {
+  $("div.inner--SODwy:lt(10)").each((index, element) => {
     const name = $(element).find("div.title--wFj93 > a").text().trim();
     const price = $(element).find("span.currency--GVKjl").text().trim();
     const image = $(element).find("img.image--WOyuZ ").attr("src");
@@ -147,7 +135,7 @@ async function daraz(query, browser) {
     products.push({ name, price, orignalprice, image, link, site });
   });
 
-  // await browser.close();
+  console.timeEnd("Daraz");
 
   return products;
 }
@@ -155,6 +143,8 @@ async function daraz(query, browser) {
 /////////////////////////////////////////////////////////////////////////////////////
 
 async function ishopping(query, browser) {
+  console.log("ishopping started");
+  console.time("ishopping");
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
 
@@ -167,22 +157,16 @@ async function ishopping(query, browser) {
   // await page.setViewport({ width: width });
   await page.setUserAgent("UA-TEST");
 
-  const url = "https://www.ishopping.pk/";
+  const url = `https://www.ishopping.pk/catalogsearch/result/?q=${query}`;
   await page.goto(url);
 
-  // waiting for search bar to load
-  await page.waitForSelector("input#search");
-  await page.type("input#search", query);
-  await page.keyboard.press("Enter");
-
-  await page.waitForNavigation();
   const html = await page.content();
-  const $ = cheerio.load(html);
+  const $ = load(html);
   await page.waitForSelector("li.col-xs-6.col-sm-4.col-md-3.item");
 
   const products = [];
 
-  $("li.col-xs-6.col-sm-4.col-md-3.item ").each((index, element) => {
+  $("li.col-xs-6.col-sm-4.col-md-3.item:lt(10) ").each((index, element) => {
     const name = $(element).find("h2.product-name > a").text();
     const price = $(element)
       .find("p.special-price > span.price, span.regular-price > span.price")
@@ -197,8 +181,8 @@ async function ishopping(query, browser) {
     const site = "Ishopping";
     products.push({ name, price, oldprice, image, link, site });
   });
-  //   console.log(JSON.stringify(products, null, 2));
 
-  // await browser.close();
+  console.timeEnd("ishopping");
+
   return products;
 }
